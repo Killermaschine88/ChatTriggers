fbbbdb82d3bd394b0a1384e1df15469f1e659a0a
Baltraz <ybaltraz@gmail.com>
2022-05-03 14:32:04 +0000

    error handling

--- SkyblockUtilsV2/features/chat.js
+++ SkyblockUtilsV2/features/chat.js
@@ -20,6 +20,9 @@ register("chat", (message, event) => {
   if (Settings.autoDetectWaypoints && !message.includes("!")) {
     const matches = message.match(/X: (-?\d*\.?\d*?) Y: (-?\d*\.?\d*?) Z: (-?\d*\.?\d*?)/);
     let x = matches[1], y = matches[2], z= matches[3];
+
+    if(!x || !y || !z) return ChatLib.chat(`${Const.prefix} Something went wrong while extracting Coordinates from chat message`)
+      
     waypoints.push({ x: x, y: y, z: z });
     ChatLib.chat(`${Const.prefix} Added a Waypoint at X: ${x}, Y: ${y}, Z: ${z}!`);
   }
