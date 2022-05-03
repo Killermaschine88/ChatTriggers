5de0a9588b9cd9779686bb04e5b6171c31f70969
Baltraz <ybaltraz@gmail.com>
2022-05-03 09:01:37 +0000

    fix mybe

--- SkyblockUtilsV2/features/chat.js
+++ SkyblockUtilsV2/features/chat.js
@@ -14,16 +14,16 @@ register("chat", (message, event) => {
 
   //Vanquisher Message
   if (Settings.vanquisherMessage && message.includes("A Vanquisher is spawning")) {
-    ChatLib.command(`pc Vanquisher spawning at: X: ${Player.getX().toFixed(2)} Y: ${Player.getY().toFixed(2)} Z: ${Player.getZ().toFixed(2)}`);
+    ChatLib.command(`pc Vanquisher spawning at: X: ${Player.getX().toFixed(2)} Y: ${Player.getY().toFixed(2)} Z: ${Player.getZ().toFixed(2)} !`);
   }
 
   //Auto Waypoint Pickup
-  if (Settings.autoDetectWaypoints && !message.includes("!")) {
+  if (Settings.autoDetectWaypoints && message.includes("!")) {
     const matches = message.match(/X: (-?\d*\.\d*) Y: (-?\d*\.\d*) Z: (-?\d*\.\d*)/);
     let x = matches[1];
     let y = matches[2];
     let z = matches[3];
     waypoints.push({ x: x, y: y, z: z });
-    ChatLib.chat(`${Const.prefix} Added a Waypoint at X: ${x}, Y: ${y}, Z: ${z}!`)
+    ChatLib.chat(`${Const.prefix} Added a Waypoint at X: ${x}, Y: ${y}, Z: ${z}`)
   }
 }).setCriteria("${message}");
