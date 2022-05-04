b172c15d392dd8eb655c8de96de19e9c1d8b1b7c
Baltraz <ybaltraz@gmail.com>
2022-05-04 11:28:24 +0000

    vanquisher channel

--- SkyblockUtilsV2/data/Settings.js
+++ SkyblockUtilsV2/data/Settings.js
@@ -5,28 +5,37 @@ import * as Const from "../utils/constants";
 
 @Vigilant(Const.moduleName, `§e${Const.moduleName}`)
 class Settings {
-  
   //Nether
   @SwitchProperty({
     name: "Vanquisher Spawn Message",
     description: "Send a Message in Party Chat containing your location if a Vanquisher Spawns",
-    category: "Nether"
+    category: "Nether",
+    subcategory: "Vanquisher",
   })
   vanquisherMessage = false;
+
+  @SelectorProperty({
+    name: "Vanquisher Message Chat",
+    description: "In which chat the Vanquisher Message should be sent when you spawn one",
+    category: "Nether",
+    subcategory: "Vanquisher",
+    options: ["All Chat (Use at own Risk)", "Party Chat"],
+  })
+  vanquisherChat = 1;
 
   @SwitchProperty({
     name: "Banner of Corruption Timer",
     description: "Starts a Timer when you place a Banner and reminds you when the banner expires",
     category: "Nether",
-    subcategory: "Corrupted Banner"
+    subcategory: "Corrupted Banner",
   })
   bannerTimer = true;
-  
+
   @TextProperty({
     name: "Banner of Corruption Display Text",
     description: "The Text to Display when the Banner of Corruption has expired ('Banner' by default)",
     category: "Nether",
-    subcategory: "Corrupted Banner"
+    subcategory: "Corrupted Banner",
   })
   bannerText = "Banner";
 
@@ -34,7 +43,7 @@ class Settings {
     name: "Banner of Corruption Display Color",
     description: "The Color of the Banner Reminder Message",
     category: "Nether",
-    subcategory: "Corrupted Banner"
+    subcategory: "Corrupted Banner",
   })
   bannerColor = Color.MAGENTA;
 
@@ -42,21 +51,21 @@ class Settings {
   @SwitchProperty({
     name: "Waypoint shown",
     description: "If Waypoints you add should be shown in-game",
-    category: "Waypoint"
+    category: "Waypoint",
   })
   waypointShown = false;
 
   @SwitchProperty({
     name: "Auto Waypoint Pickup",
     description: "If a Waypoint is detected in chat it will automatically be rendered (Only works if Waypoint shown is enabled)",
-    category: "Waypoint"
+    category: "Waypoint",
   })
   autoDetectWaypoints = false;
 
   @SwitchProperty({
     name: "Display Waypoint Distance",
     description: "Displays how far away you are from a waypoint (Only works if Waypoint shown is enabled)",
-    category: "Waypoint"
+    category: "Waypoint",
   })
   displayWaypointDistance = false;
 
@@ -64,7 +73,7 @@ class Settings {
   @SwitchProperty({
     name: "Phrase Filter",
     description: "Enable/Disable a filter for certain Phrases or Words from a list you make using /su word-list add myWord",
-    category: "Filter"
+    category: "Filter",
   })
   phraseFilter = false;
 
@@ -101,7 +110,7 @@ class Settings {
     name: "API Key",
     description: "API Key given out by Baltraz#4874 to access the Flipper (Only like 3 people get one so yea)",
     category: "Flipper",
-    placeholder: "None"
+    placeholder: "None",
   })
   apiKey = "";
 
