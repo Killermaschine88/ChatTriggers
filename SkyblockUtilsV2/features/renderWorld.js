e52314d4fda975f8df0d0ecff3e5754462125073
Baltraz <ybaltraz@gmail.com>
2022-05-04 07:03:52 +0000

    fix

--- SkyblockUtilsV2/features/renderWorld.js
+++ SkyblockUtilsV2/features/renderWorld.js
@@ -5,7 +5,7 @@ register("renderWorld", () => {
   if (!Settings.waypointShown) return;
   waypoints.forEach((waypoint) => {
     if (Settings.displayWaypointDistance) {
-      Tessellator.drawString(`Vanquisher, ${Math.floor(Math.hypot(waypoint.x - Player.getX(), waypoint.y - Player.getY(), waypoint.z - Player.getZ()))}`, waypoint.x, waypoint.y, waypoint.z, 0xffffff, true, 1, true);
+      Tessellator.drawString(`Vanquisher, ${Math.floor(Math.hypot(waypoint.x - Player.getX(), waypoint.y - Player.getY(), waypoint.z - Player.getZ()))}m`, waypoint.x, waypoint.y, waypoint.z, 0xffffff, true, 1, true);
     } else {
       Tessellator.drawString(`Vanquisher`, waypoint.x, waypoint.y, waypoint.z, 0xffffff, true, 1, true);
     }
