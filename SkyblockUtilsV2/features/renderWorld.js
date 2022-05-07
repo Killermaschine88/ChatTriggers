a311c748a7f756220b476c045e77fa96afdedb75
Baltraz <ybaltraz@gmail.com>
2022-05-07 19:08:32 +0200

    Update renderWorld.js

--- SkyblockUtilsV2/features/renderWorld.js
+++ SkyblockUtilsV2/features/renderWorld.js
@@ -3,12 +3,17 @@ import * as MathUtils from "./../utils/MathUtils";
 global.waypoints = [];
 
 register("renderWorld", () => {
-  if (!Settings.waypointShown) return;
-  waypoints.forEach((waypoint) => {
-    if (Settings.displayWaypointDistance) {
-      Tessellator.drawString(`Vanquisher, ${MathUtils.getWaypointDistance(waypoint)}m`, waypoint.x, waypoint.y, waypoint.z, 0xffffff, true, 1, true);
-    } else {
-      Tessellator.drawString(`Vanquisher`, waypoint.x, waypoint.y, waypoint.z, 0xffffff, true, 1, true);
-    }
-  });
+  if (Settings.waypointShown) {
+    waypoints.forEach((waypoint) => {
+      if (Settings.displayWaypointDistance) {
+        Tessellator.drawString(`Vanquisher, ${MathUtils.getWaypointDistance(waypoint)}m`, waypoint.x, waypoint.y, waypoint.z, 0xffffff, true, 1, true);
+      } else {
+        Tessellator.drawString(`Vanquisher`, waypoint.x, waypoint.y, waypoint.z, 0xffffff, true, 1, true);
+      }
+    });
+  }
+
+	damageText.forEach((dmg) => {
+		Tessellator.drawString(`${dmg.name}`, dmg.x, dmg.y, dmg.z, 0xffffff, true, 1, true);
+	})
 });
